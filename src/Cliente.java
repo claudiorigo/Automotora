d7a97cbb014af52dce4aab6ba0a893139d8a2c2a
Sergio Araya <Sergio Araya@DESKTOP-E0SE3OR>
2020-09-24 05:47:31 -0300

    for/while/do while/if else/switch/Scanner

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,18 +1,22 @@
+import java.util.Scanner;
 
 public class Cliente {
     private String rut,nombre,correo;
-    private int telefono;
+    private int telefono, edad;
     private char preferencial;
     private Automovil automovil;
-
+    private Scanner leer = new Scanner(System.in);
+    
+    //Constructor sin parametros
     public Cliente() {
     }
-
-    public Cliente(String rut, String nombre, String correo, int telefono, char preferencial, Automovil automovil) {
+    //Constructor con parametros
+    public Cliente(String rut, String nombre, String correo, int telefono, int edad, char preferencial, Automovil automovil) {
         this.rut = rut;
         this.nombre = nombre;
         this.correo = correo;
         this.telefono = telefono;
+        this.edad = edad;
         //this.preferencial = preferencial;
         this.setPreferencial(preferencial);
         this.automovil = automovil;
@@ -49,7 +53,15 @@ public class Cliente {
     public void setTelefono(int telefono) {
         this.telefono = telefono;
     }
-
+        
+    public int getEdad(){
+        return edad;
+    }
+    
+    public void setEdad(int edad){
+        this.edad = edad;
+    }
+    
     public char getPreferencial() {
         return preferencial;
     }
@@ -72,16 +84,80 @@ public class Cliente {
 
    
     public void imprimirCliente(){
-        System.out.println("Nombre Cliente: " + this.nombre);
-        System.out.println("Rut: " + this.rut);
-        System.out.println("Correo: " + this.correo);
-        System.out.println("Telefono " + this.telefono);
+        System.out.println("Nombre Cliente: " + this.getNombre());
+        System.out.println("Rut: " + this.getRut());
+        System.out.println("Edad: " + this.getEdad());
+        System.out.println("Correo: " + this.getCorreo());
+        System.out.println("Telefono " + this.getTelefono());
         System.out.println("Cliente Tipo: " + this.getPreferencial());
         System.out.println("Numero Chasis: " + this.automovil.getNumeroChasis());
         System.out.println("Kilometro " + this.automovil.getKilometro());
         System.out.println("Valor Vehiculo " + Math.round(this.automovil.getValor()));
         System.out.println("Cargo Fijo " + Math.round(this.automovil.getCargoFijo()));
-        
+        System.out.println("-----------------------------------------------------------");
     }
+    //Usar condiciones, los IF  ElSE
+    public void revisarEdad(){
+        //if + tab  ó  ifelse + tab
+        if (this.edad >18 ) {
+            System.out.println("Es mayor de edad - Usando el IF/ELSE");
+            
+        } else {
+            System.out.println("No es mayor de edad - Usando el IF/ELSE");
+        }
+    }
+    //Usando condiciones, IF  ELSE anidados Switch
+    public void segunEdadEres(){
+        if (this.edad >= 18) {
+            System.out.println("Es mayor de edad - usando switch");
+        }else{
+            if (this.getEdad() > 0 && this.edad <= 4) {
+                System.out.println("Eres un bebe - usando switch");
+            }else{
+                if (this.getEdad() >= 5 && this.edad <= 13) {
+                    System.out.println("Eres un niño - usando switch");
+                }else{
+                    System.out.println("Eres Adolecente - usando switch");
+                }
+            }
+        }
+    }
+    //CICLOS WHILE - DO WHILE - FOR
+    //While
+    public void mientras(){
+        //wh + tab
+        while (edad >= 18) {
+            System.out.println("Es mayor de edad");
+            System.out.println("Ingrese su edad → ");
+            edad = leer.nextInt();
+        }        
+        System.out.println("Gracias Saliste del While");
+        System.out.println("------------------------------");
+    }
+    //for
+    public void para(){
+        //for + tab
+        int hasta;
+        System.out.println("Ingresa el fin del for: → ");
+        hasta = leer.nextInt();
+        for (int i = 0; i < hasta; i++) {
+            System.out.println("→ " + i);
+        }
+        System.out.println("Gracias fin del for");
+        System.out.println("------------------------------");
+    }
+    //Do While
+    public void repetir(){
+        //dowhile + tab
+        int opcion;
+        do {            
+            System.out.println("1. Opción 1");
+            System.out.println("2. Opción 2");
+            System.out.println("3. Salir");
+            System.out.println("Ingrese Opción: → ");
+            opcion = leer.nextInt();
+            
+        } while (opcion != 3);
+    } 
     
 }
--- src/TestAutomovil.java
+++ src/TestAutomovil.java
@@ -3,13 +3,22 @@ public class TestAutomovil {
     public static void main(String[] args) {
         Automovil auto1 = new Automovil(17000, "LS3243", 3000000, 140000);
         Automovil auto2 = new Automovil(45000, "LY1212", 5500000, 150000);
-        
-        Cliente cliente1 = new Cliente("12111650", "Claudio Rigollet", "claudiorigo@gmail.com", 954225035, 'R', auto2);
-        
-        
-        
+        Cliente cliente0 = new Cliente();
+        Cliente cliente1 = new Cliente("12111650", "Claudio Rigollet", "claudiorigo@gmail.com", 954225035, 25, 'P', auto2);
+        cliente0.setEdad(13); //Cambiar edad - 0 a 4 bebe - 5 a 13 niño - 14 a 17 adolecente - 18 mayor de edad 
+        System.out.println("_________________________________________");
+        System.out.println("Edad cliente 0 es: " + cliente0.getEdad() + " años");
+        System.out.println("_________________________________________");
+        cliente0.segunEdadEres();
+        System.out.println("_________________________________________");
         cliente1.imprimirCliente();
-        
+        cliente1.revisarEdad();
+        System.out.println("------------------------------");
+        System.out.println("Ciclos While, Do While, for");
+        cliente1.mientras();
+        cliente1.para();
+        cliente1.repetir();
+        cliente1.revisarEdad();
         
     }
 }
